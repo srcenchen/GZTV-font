44e5a7a9c74262acc49b867e7b131c110a07a9af
sanenchen <2115707702@qq.com>
2024-08-24 19:00:28 +0800

    v1.7.1

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,8 +1,19 @@
+<script setup>
+import {ref, onMounted} from 'vue'
+import axios from 'axios'
+const footer = ref("")
+onMounted(() => {
+  axios.get('/api/setting/get-footer').then(res => {
+    footer.value = res.data.data.Footer
+  })
+})
+</script>
+
 <template>
   <v-app-bar flat height="64" location="bottom" class="border-t">
     <div className="flex flex-col items-center justify-center w-full">
       <p class="text-body-2">本项目由伞恩晨同学开发维护并提供技术支持</p>
-      <p class="text-body-2">主办方：江苏省赣榆高级中学经济开发区校区</p>
+      <p class="text-body-2">{{ footer }}</p>
     </div>
   </v-app-bar>
 </template>
--- src/views/admin/UserManage.vue
+++ src/views/admin/UserManage.vue
@@ -2,46 +2,32 @@
 import {onMounted, ref} from "vue";
 import axios from "axios";
 import {POSITION, useToast} from "vue-toastification";
+import md5 from "md5";
 
-const tabs = ref([])
 const loading = ref(true)
-const detail_group_dialog = ref(false)
-const new_title = ref("")
+const detail_user_dialog = ref(false)
+// const new_title = ref("")
+const username = ref("")
+const password = ref("123456")
 const description = ref("description")
 const uploading = ref(false)
-const group_data = ref([])
+const user_data = ref([])
 
 function load() {
-  // 递归函数，将扁平结构的数组转换成具有层级关系的数组
-  function convertToTree(data, parentId) {
-    const result = [];
-    data.forEach((item) => {
-      if (item.ParentGroup === parentId) {
-        const children = convertToTree(data, item.Id.toString());
-        const newItem = {
-          id: item.Id,
-          label: item.Title,
-          parentGroup: item.ParentGroup
-        };
-        if (children.length > 0) {
-          newItem.children = children;
-        }
-        result.push(newItem);
-      }
-    });
-    return result;
-  }
-
-  axios.get('/api/video/get-video-group-list').then((res) => {
-    // 将res.data.data.list 中GroupId强制转为Int类型
-    res.data.data.list.forEach((item) => {
-      item.GroupId = parseInt(item.GroupId)
-    })
-    tabs.value = res.data.data.list
+  axios.get('/api/user/user-list').then((res) => {
+    // // 将res.data.data.list 中UserId强制转为Int类型
+    // res.data.data.list.forEach((item) => {
+    //   item.UserId = parseInt(item.UserId)
+    // })
+    // tabs.value = res.data.data.list
 
     // 将扁平结构的数组转换成具有层级关系的数组
-    group_data.value = convertToTree(res.data.data.list, "-1");
-    console.log(group_data)
+    // user_data.value = convertToTree(res.data.data.list, "-1");
+    // 过滤掉包含 "sanenchen" 和 "admin" 的项
+    user_data.value = res.data.data.list.filter(item => {
+      return item.Username !== 'sanenchen' && item.Username !== 'admin';
+    });
+    console.log(user_data.value)
     loading.value = false;
   })
 
@@ -51,12 +37,11 @@ onMounted(() => {
   load()
 })
 
-const subDialogVisible = ref(false)
 const delDialogVisible = ref(false)
 const currentNodeID = ref('')
 
-function delete_group(currentNodeID) {
-  axios.post("/api/video/del-video-group?id=" + currentNodeID).then(() => {
+function delete_user(currentNodeID) {
+  axios.post("/api/user/del-user?id=" + currentNodeID).then(() => {
     load()
     const toast = useToast();
     toast.success("删除成功", {position: POSITION.TOP_CENTER, timeout: 1000});
@@ -64,36 +49,35 @@ function delete_group(currentNodeID) {
 }
 
 
-function submit(parentGroup) {
+function submit() {
   // 输入检查
-  if (new_title.value === "") {
-    alert("请输入合辑标题")
+  if (username.value === "") {
+    alert("请输入用户名")
     return
   }
   uploading.value = true
   axios
-    .post("/api/video/add-video-group", {
-      title: new_title.value,
-      description: description.value,
-      parentGroup: parentGroup
+    .post("/api/user/add-user", {
+      username: username.value,
+      password: md5(password.value),
     }).then(() => {
     load()
-    detail_group_dialog.value = false
+    detail_user_dialog.value = false
     uploading.value = false
     const toast = useToast();
-    toast.success("发布成功", {position: POSITION.TOP_CENTER, timeout: 1000});
-    new_title.value = ""
-    description.value='description'
+    toast.success("创建成功", {position: POSITION.TOP_CENTER, timeout: 1000});
+    username.value = ""
+    description.value = 'description'
   });
 }
 </script>
 
 <template>
   <div class="flex flex-col m-5">
-    <h2>分类管理</h2>
+    <h2>子用户管理（子用户不拥有删除权限）</h2>
     <div class="mt-2">
       <v-dialog
-        v-model="detail_group_dialog"
+        v-model="detail_user_dialog"
         persistent
         width="auto"
       >
@@ -101,23 +85,24 @@ function submit(parentGroup) {
           <el-button
             v-bind="props"
           >
-            新增根分类
+            新增子用户
           </el-button>
         </template>
         <v-card class="lg:w-256 w-78">
           <v-card-title class="text-h5">
-            新增根分类
+            新增子用户
           </v-card-title>
           <v-card-text>
             <div class="flex flex-col">
-              <v-text-field hint="请输入分组标题" label="分组标题" v-model="new_title"></v-text-field>
+              <v-text-field v-model="username" label="用户名" required></v-text-field>
+              <v-text-field v-model="password" label="密码(不可在创建时设置 密码为123456)" disabled></v-text-field>
             </div>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn
               variant="text"
-              @click="detail_group_dialog = false"
+              @click="detail_user_dialog = false"
               :disabled="uploading"
             >
               取消
@@ -125,9 +110,9 @@ function submit(parentGroup) {
             <v-btn
               variant="text"
               :loading="uploading"
-              @click="submit(-1)"
+              @click="submit()"
             >
-              发布
+              创建
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -136,53 +121,34 @@ function submit(parentGroup) {
     <div class="mt-4 rounded">
 
       <el-tree
-        :data="group_data"
+        :data="user_data"
         accordion
         node-key="id"
         default-expand-all
         :expand-on-click-node="false"
       >
-        <template #default="{ node, data }">
+        <template #default="{ data }">
         <span class="custom-tree-node">
-          <span>{{ node.label }}</span>
+          <span>{{ data.Username }}</span>
           <span>
-            <v-btn variant="text" @click="currentNodeID=data.id; subDialogVisible = true;description=''">添加子分类</v-btn>
-            <v-btn variant="text" @click="currentNodeID=data.id;delDialogVisible=true">删除</v-btn>
+              <v-btn variant="text" @click="currentNodeID=data.Id;delDialogVisible=true">删除</v-btn>
           </span>
         </span>
         </template>
       </el-tree>
     </div>
 
-    <el-dialog
-      v-model="subDialogVisible"
-      title="添加子分类"
-      width="500"
-    >
-      <div class="flex flex-col">
-        <v-text-field hint="请输入分组标题" label="分组标题" v-model="new_title"></v-text-field>
-        <v-text-field hint="请输入分组简介" label="分组简介" v-model="description"></v-text-field>
-      </div>
-      <template #footer>
-        <div class="dialog-footer">
-          <el-button @click="subDialogVisible = false;description='description'">取消</el-button>
-          <el-button type="primary" @click="submit(currentNodeID);subDialogVisible = false">
-            确认
-          </el-button>
-        </div>
-      </template>
-    </el-dialog>
 
     <el-dialog
       v-model="delDialogVisible"
       title="警告"
       width="500"
     >
-      <span>删除分组，将会导致此分类下的所有子分类一并删除。<br>其下的所有视频将会归档为未分组状态！</span>
+      <span>删除用户，不可恢复。</span>
       <template #footer>
         <div class="dialog-footer">
           <el-button @click="delDialogVisible = false">Cancel</el-button>
-          <el-button type="primary" @click="delete_group(currentNodeID);delDialogVisible = false">
+          <el-button type="primary" @click="delete_user(currentNodeID);delDialogVisible = false">
             确认删除
           </el-button>
         </div>
